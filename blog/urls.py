8a90c304e497addcff8e33f5e128e74e05927c7d
sirehassan <htshongany@gmail.com>
2022-07-21 00:00:55 +0000

    addition of a category system

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import home , ListPosts , DetailPost 
+from .views import home , ListPosts , DetailPost , category_view
 
 
 urlpatterns = [
     path('home/', home , name="home"),
     path('', ListPosts.as_view() , name='listposts'),
     path('article/<int:pk>', DetailPost.as_view() , name='detailpost'),
+    path('article/category/<int:cats>', category_view , name='listcategory' )
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , get_object_or_404
 from django.views.generic import ListView , DetailView
 from .models import BlogPost , Category
+from django.core.paginator import Paginator
 # Create your views here.
 
 def home(request):
@@ -11,6 +12,21 @@ def home(request):
 	}
 
 	return render(request, 'home.html', context)
+
+def category_view(request , cats):
+
+	get_object_or_404(Category.objects.filter(id=cats))
+
+	paginator = Paginator(BlogPost.objects.filter(category=cats),2)
+
+	context = {
+	'page_obj': paginator.get_page(request.GET.get('page'))	
+
+	}
+
+
+
+	return render(request, 'category.html', context)
 
 class ListPosts(ListView):
 	model = BlogPost
