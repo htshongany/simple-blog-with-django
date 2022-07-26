e24e52bb6807e85f221d42c6faf1e52b00f48b79
sirehassan <htshongany@gmail.com>
2022-07-26 00:48:32 +0000

    set views add search and del home

--- blog/views.py
+++ blog/views.py
@@ -4,39 +4,32 @@ from .models import BlogPost , Category
 from django.core.paginator import Paginator
 # Create your views here.
 
-# def home(request):
-# 	context = {
-# 	'posts':BlogPost.objects.all(),
-# 	'category': Category.objects.all()
-# 	}
-
-# 	return render(request, 'home.html', context)
 
 def category_view(request , cats):
 
-	get_object_or_404(Category.objects.filter(id=cats))
+    get_object_or_404(Category.objects.filter(id=cats))
 
-	paginator = Paginator(BlogPost.objects.filter(category=cats,published=True),2)
+    paginator = Paginator(BlogPost.objects.filter(category=cats,published=True),2)
 
-	context = {
-	'object_list': paginator.get_page(request.GET.get('page')),
-	'category': Category.objects.all(),
-	
-	}
+    context = {
+    'object_list': paginator.get_page(request.GET.get('page')),
+    'category': Category.objects.all(),
+    
+    }
 
-	return render(request, 'blog/category.html', context)
+    return render(request, 'blog/category.html', context)
 
 def list_post_view(request):
 
-	paginator = Paginator(BlogPost.objects.filter(published=True),2)
-	context = {
-	'object_list': paginator.get_page(request.GET.get('page')),
-	'category': Category.objects.all(),
+    paginator = Paginator(BlogPost.objects.filter(published=True),2)
+    context = {
+    'object_list': paginator.get_page(request.GET.get('page')),
+    'category': Category.objects.all(),
 
-	}
-	return render(request, 'blog/list.html', context)
+    }
+    return render(request, 'blog/list.html', context)
 
 
 class DetailPost(DetailView):
-	model = BlogPost
-	template_name = "blog/detail.html"
+    model = BlogPost
+    template_name = "blog/detail.html"
