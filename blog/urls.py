b17a0a79337b6daa70d07612348b8ce32590b540
sirehassan <htshongany@gmail.com>
2022-07-27 01:36:58 +0000

    add search system

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import list_post_view , category_view , DetailPost
+from .views import list_post_view , category_view , DetailPost , search_view
 
 
 urlpatterns = [
     path('', list_post_view , name='listposts'),
     path('article/<int:pk>', DetailPost.as_view() , name='detailpost'),
-    path('article/category/<int:cats>', category_view , name='listcategory' )
+    path('article/category/<int:cats>', category_view , name='listcategory' ),
+    path("search/", search_view, name="search-bar"),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render , get_object_or_404
 from django.views.generic import ListView , DetailView
 from .models import BlogPost , Category
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator , EmptyPage, PageNotAnInteger
 # Create your views here.
+
+POSTS_PER_PAGE = 2
+
+def search_view(request):
+    object_list , search  = _search_posts(request)
+    context = {"search": search , 'object_list':object_list}
+    
+    return render(request, "blog/search.html",context)
 
 
 def category_view(request , cats):
 
     get_object_or_404(Category.objects.filter(id=cats))
 
-    paginator = Paginator(BlogPost.objects.filter(category=cats,published=True),2)
+    paginator = Paginator(BlogPost.objects.filter(category=cats,published=True),POSTS_PER_PAGE)
 
     context = {
     'object_list': paginator.get_page(request.GET.get('page')),
@@ -21,7 +29,7 @@ def category_view(request , cats):
 
 def list_post_view(request):
 
-    paginator = Paginator(BlogPost.objects.filter(published=True),2)
+    paginator = Paginator(BlogPost.objects.filter(published=True),POSTS_PER_PAGE)
     context = {
     'object_list': paginator.get_page(request.GET.get('page')),
     'category': Category.objects.all(),
@@ -29,7 +37,25 @@ def list_post_view(request):
     }
     return render(request, 'blog/list.html', context)
 
-
 class DetailPost(DetailView):
     model = BlogPost
     template_name = "blog/detail.html"
+
+def _search_posts(request):
+
+    search = request.GET.get("search")
+    page = request.GET.get("page")
+    object_list = BlogPost.objects.all()
+
+    if search:
+        object_list = object_list.filter(title__icontains=search)
+
+    paginator = Paginator(object_list, POSTS_PER_PAGE)
+    try:
+        object_list = paginator.page(page)
+    except PageNotAnInteger:
+        object_list = paginator.page(1)
+    except EmptyPage:
+        object_list = paginator.page(paginator.num_pages)
+
+    return object_list , search or ""
