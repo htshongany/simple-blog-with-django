af74be43c8ec58e975a72a4d739d8590b045cea8
sirehassan <htshongany@gmail.com>
2022-08-21 12:06:23 -0800

    set models unittest

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
 from blog.models import BlogPost
+from django.utils import timezone
 
 class TestModel(TestCase):
-		
+	
+	def setUp(self):
+		self.timezone = timezone.now().date()
+
 	def test_blog_model_post_is_create(self):
 
 		blog_post = BlogPost.objects.create(
@@ -12,3 +16,5 @@ class TestModel(TestCase):
 			)
 
 		self.assertEqual(blog_post.published, False)
+		self.assertEquals(blog_post.update_date, self.timezone)
+		self.assertEquals(blog_post.pub_date, self.timezone)
